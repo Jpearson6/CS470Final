279a474b655367d3188432446eb1ae3472695d0f
depriest-jt <jtonkeys@gmail.com>
2023-04-04 15:59:31 -0700

    Hamburger Menu
    The hamburger menu now exists, though only the homepage option exists. It's basically just the one we used in project 4.

---
+++ app-ui/src/Components/Homepage.js
@@ -0,0 +1,7 @@
+import {Typography} from "@mui/material";
+
+export default function Homepage() {
+    return <Typography>
+        Hello World!
+    </Typography>
+}
---
+++ app-ui/src/Menu/MenuPresentationComponents.js
@@ -0,0 +1,12 @@
+import Homepage from "../Components/Homepage";
+
+const presentationComponents = () => {
+    return [
+        {
+            title: 'Homepage',
+            component: <Homepage />
+        }
+    ];
+};
+
+export {presentationComponents}
